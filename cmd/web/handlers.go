f799506c51589bbdb9ce2b72bab47e52ba2606fa
Taiwo Adetunji <dontiwo5020@yahoo.com>
2026-06-22 04:29:19 +0100

    Added new files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,59 +2,86 @@ package main
 
 import (
   "fmt"
-  "log"
+  "errors"
   "net/http"
   "strconv"
-  "html/template"
+//   "html/template"
+  "github.com/Devtiwo/snippetbox/internal/models"
 )
 
 // Creates a home page handler function.
-func home(w http.ResponseWriter, r *http.Request) {
+// Changing the structure of the home handler so it is defined as a method against *application.
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
   if r.URL.Path != "/" {
-	http.NotFound(w, r)
+	app.notFound(w) // Using the notFound helper method to send a 404 Not Found response to the user if the URL path is not "/".
 	return
   }
-
+  snippets, err := app.snippets.Latest()
+  if err != nil {
+    app.serverError(w, err)
+   return
+  }
+  for _, snippet := range snippets {
+    fmt.Fprintf(w, "%+v\n", snippet)
+  }
   // Initailizing a slice containing the paths of the template files to be parsed
   // The base file must come first, followed by the other template files.
-  files := []string {
-	"./ui/html/base.tmpl",
-	"./ui/html/partials/nav.tmpl",
-	"./ui/html/pages/home.tmpl",
-  }
+//   files := []string {
+// 	"./ui/html/base.tmpl",
+// 	"./ui/html/partials/nav.tmpl",
+// 	"./ui/html/pages/home.tmpl",
+//   }
 
-  // using the template.ParseFiles() function to read the template file and storing it into a template set.
-  ts, err := template.ParseFiles(files...)
-  if err != nil {
-	log.Print(err.Error())
-	http.Error(w, "Internal server error", 500)
-    return
-  }
+//   // using the template.ParseFiles() function to read the template file and storing it into a template set.
+//   ts, err := template.ParseFiles(files...)
+//   if err != nil {
+// 	app.serverError(w, err)
+//     return
+//   }
   
-  // Using the ExecuteTemplate() method on the template set to write the template content as the response body.
-  // The second parameter in the Execute() methos is used to pass dynamic data to the template.
-  err = ts.ExecuteTemplate(w, "base", nil)
-  if err != nil {
-	log.Print(err.Error())
-	http.Error(w, "Internal server error", 500)
-  }}
+//   // Using the ExecuteTemplate() method on the template set to write the template content as the response body.
+//   // The second parameter in the Execute() methos is used to pass dynamic data to the template.
+//   err = ts.ExecuteTemplate(w, "base", nil)
+//   if err != nil {
+// 	app.serverError(w, err)
+  }
 
 // Creates a snippetview handler function.
-func snippetView(w http.ResponseWriter, r *http.Request) {
+// Changing the structure of the snippetView handler so it is defined as a method against *application.
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
   id, err:= strconv.Atoi(r.URL.Query().Get("id"))
   if err != nil || id < 1 {
-	http.NotFound(w, r)
+	app.notFound(w) // Using the notFound helper.
 	return
   }
-  fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+  snippet, err := app.snippets.Get(id)
+  if err != nil {
+	if errors.Is(err, models.ErrNoRecord) {
+	  app.notFound(w)
+	} else {
+	  app.serverError(w, err)
+	}
+	return
+  }
+  fmt.Fprintf(w, "%+v", snippet)
 }
 
 // Creates a snippetCreate handler function.
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+// Changing the structure of the snippetCreate handler so it is defined as a method against *application.
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
   if r.Method != "POST" {
 	w.Header().Set("Allow", http.MethodPost)
-	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	app.clientError(w, http.StatusMethodNotAllowed) // Using the clientError helper
 	return
   }
+  title := "O snail"
+  content := "O snail\nClimb Mount Fuji, \nBut slowly, slowly!\n\n- Kobayashi Issa"
+  expires := 7
+  id, err := app.snippets.Insert(title, content, expires)
+  if err != nil {
+	app.serverError(w, err)
+	return
+  }
+  http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
   w.Write([]byte("Creates a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,24 @@ import (
   "flag"
   "log"
   "net/http"
+  "database/sql"
+  "github.com/Devtiwo/snippetbox/internal/models"
+  _ "github.com/go-sql-driver/mysql"
 )
+
+// Defining an application struct to hold the dependencies for our web app.
+type application struct {
+  errorLog *log.Logger
+  infoLog *log.Logger
+  snippets *models.SnippetModel
+}
 
 func main() {
     // Using the flag package to define a command-line flag named "addr" with a default value of ":4000" and a description of "HTTP network address".
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Using the flag package to define a command-line flag for mysql dsn string.
+	dsn := flag.String("dsn", "web:adewunmi5020@/snippetbox?parseTime=true", "MySQL data source name")
 
 	// Calling the flag.Parse() function to parse the command-line flags and store the values in the corresponding variables.
 	flag.Parse()
@@ -20,23 +33,45 @@ func main() {
 
 	// Create a logger for writing error messages in the same way, but usestderr as the destination and use the log.Lshortfile flag to include the relevant file name and line number.
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
-    mux := http.NewServeMux()
     
-	// Creating a file server to serve static files from the "./ui/static/" directory.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-    // Using the mux.Handle() method to register the file server handler for the "/static/" route.
-    // Using the http.StripPrefix() function to remove the "/static" prefix from the request URL path before passing it to the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// Registering the handler functions for different routes.
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	
-	infoLog.Printf("starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	// Calling the openDB() function to create a connection pool for the MySQL database.
+	db, err := openDB(*dsn)
 	if err != nil {
 	  errorLog.Fatal(err)
 	}
+
+    // Using the defer statement to ensure that the database connection pool is closed before the main() function exits.
+	defer db.Close()
+	// initializing a new instance of the application struct containing the dependencies.
+	app := &application{
+	  errorLog: errorLog,
+	  infoLog: infoLog,
+	  snippets: &models.SnippetModel{DB: db},
+	}
+
+	// Initialize a new http.Server struct. We set the Addr and Handler fields so that the server uses the same network address and routes before and set the errorlog field so that the server now uses the custome errorlog logger in the event of any problems.
+	srv := &http.Server{
+	  Addr: *addr,
+	  ErrorLog: errorLog,
+	  Handler: app.routes(), // Calling the new app.routes() method to get the servemux containing our routes.
+	}
+
+	infoLog.Printf("starting server on %s", *addr)
+
+	// Call the ListenAndServe() method on our new http.Server struct.
+	// Since the err variable is already declared, we can use the assignment operator to assign the return value of the ListenAndServe() method to it.
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+// The openDB() function wraps sql.Open() and returns a sql.DB connection pool for a given DSN.
+func openDB(dsn string) (*sql.DB, error) {
+  db, err := sql.Open("mysql", dsn)
+  if err != nil {
+	return nil, err 
+  }
+  if err = db.Ping(); err != nil {
+	return nil, err
+  }
+  return db, nil
 }
